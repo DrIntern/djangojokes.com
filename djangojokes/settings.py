b762fd2da52bc4115ec67ab3a3ae1e9c3db94598
Liv <jcox0893@live.com>
2026-04-09 17:52:21 -0500

    lesson2part2

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # built into django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local 
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
