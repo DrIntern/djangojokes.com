e4c9e898edb03485ca08416ac7c4988fee9cc963
Liv <jcox0893@live.com>
2026-04-16 18:33:24 -0500

    lesson8part14

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 from datetime import datetime
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+def validate_future_date(value):
+    if value < datetime.now().date():
+        raise ValidationError(
+            message=f'{value} is in the past.', code='past_date'
+        )
 
 class JobApplicationForm(forms.Form):
 
@@ -10,11 +18,11 @@ class JobApplicationForm(forms.Form):
     )
 
     week = (
-        ('1', 'Monday'),
-        ('2', 'Tuesday'),
-        ('3', 'Wednesday'),
-        ('4', 'Thursday'),
-        ('5', 'Friday'),
+        ( 1, 'Monday'),
+        ( 2, 'Tuesday'),
+        ( 3, 'Wednesday'),
+        ( 4, 'Thursday'),
+        ( 5, 'Friday'),
     )
 
     years = range(datetime.now().year, datetime.now().year+2)
@@ -23,27 +31,34 @@ class JobApplicationForm(forms.Form):
         widget = forms.TextInput(attrs= {'autofocus': True})
     )
     last_name = forms.CharField()
+
     email = forms.EmailField()
-    website = forms.URLField(required = False,
-        widget = forms.URLInput(
-            attrs= {
-                'placeholder' : 'htttps:/fakewebsite.com',
-                'size' : '50',
-            }
-        ) 
+
+    website = forms.CharField(
+        required= False,
+        validators=[URLValidator(schemes=['http', 'https'])],
+        widget= forms.TextInput(attrs= {
+            'placeholder' : 'https://fakewebsite.com'
+        })
     )
+    
     employment_type = forms.ChoiceField(choices=employment)
+
     start_date = forms.DateField(help_text='The earliest date you can start working.', 
         widget = forms.SelectDateWidget(
-            years= years
-        )                
+            years= years,
+            attrs= {'style': 'width: 31%; display: inline-block; margin: 0 1%'}
+        ),
+        validators=[validate_future_date],
+        error_messages = {'past_date': 'Please enter a future date.'}
     )
-    availability = forms.MultipleChoiceField(choices = week,
-        widget = forms.CheckboxSelectMultiple(
-            attrs= {
-                'checked' : 'True'
-            }
-        )                                    
+    availability = forms.TypedMultipleChoiceField(
+        choices=week,
+        coerce=int,
+        help_text='Check all days that you can work.',
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked':True}
+        )
     )
     desired_hourly_rate = forms.DecimalField(
         widget= forms.NumberInput(
