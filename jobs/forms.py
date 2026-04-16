0737009321ca1f9526f7e910d4166e1528a781b1
Liv <jcox0893@live.com>
2026-04-16 17:34:44 -0500

    lesson8part11

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from datetime import datetime
 
 class JobApplicationForm(forms.Form):
 
@@ -16,13 +17,49 @@ class JobApplicationForm(forms.Form):
         ('5', 'Friday'),
     )
 
-    first_name = forms.CharField()
+    years = range(datetime.now().year, datetime.now().year+2)
+
+    first_name = forms.CharField(
+        widget = forms.TextInput(attrs= {'autofocus': True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(required = False)
+    website = forms.URLField(required = False,
+        widget = forms.URLInput(
+            attrs= {
+                'placeholder' : 'htttps:/fakewebsite.com',
+                'size' : '50',
+            }
+        ) 
+    )
     employment_type = forms.ChoiceField(choices=employment)
-    start_date = forms.DateField(help_text='The earliest date you can start working.')
-    availability = forms.MultipleChoiceField(choices = week)
-    desired_hourly_rate = forms.DecimalField()
-    cover_letter = forms.CharField(required= False)
+    start_date = forms.DateField(help_text='The earliest date you can start working.', 
+        widget = forms.SelectDateWidget(
+            years= years
+        )                
+    )
+    availability = forms.MultipleChoiceField(choices = week,
+        widget = forms.CheckboxSelectMultiple(
+            attrs= {
+                'checked' : 'True'
+            }
+        )                                    
+    )
+    desired_hourly_rate = forms.DecimalField(
+        widget= forms.NumberInput(
+            attrs= {
+                'min' : '10.00',
+                'max' : '100.00',
+                'step' : '.25',
+            }
+        )
+    )
+    cover_letter = forms.CharField(required= False,
+        widget= forms.Textarea(
+            attrs= {
+                'cols' : '75',
+                'rows' : '5',
+            }
+        ) 
+    )
     confirmation = forms.BooleanField(label='I certify that the information I have provided is true.')
