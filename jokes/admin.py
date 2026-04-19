de1bf677f2edfcb98dc8f4d72d748b5a45781e3f
Liv <jcox0893@live.com>
2026-04-19 16:17:39 -0500

    lesson9part12

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Joke
+from .models import Category, Joke, Tag
 
 # Register your models here.
 
@@ -7,6 +7,29 @@ from .models import Category, Joke
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
     list_display = ['category', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+
+
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Joke
+    list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+
+        return ()
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
