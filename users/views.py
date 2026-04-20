3206496b14e66e1e6fa1b73152739f14f9973a21
Liv <jcox0893@live.com>
2026-04-20 18:56:36 -0500

    lesson11part5

--- users/views.py
+++ users/views.py
@@ -4,15 +4,17 @@ from django.views.generic import UpdateView
 from allauth.account.views import PasswordChangeView
 from .forms import CustomUserChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
 
 class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy('my-account')
 
-class MyAccountPageView(LoginRequiredMixin, UpdateView):
+class MyAccountPageView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
+    success_message = 'Great Success!'
     template_name = 'account/my_account.html'
 
     def get_object(self):
