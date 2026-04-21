3a3adbefc24ab19bfd8b1ccd2cd9403c0133221f
Liv <jcox0893@live.com>
2026-04-20 19:18:37 -0500

    lesson12part3

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -182,6 +182,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 if os.environ.get('ENVIRONMENT') != 'production':
     from .local_settings import *
 
