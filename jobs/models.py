7bbb9765161ff3b351e938a19aced4a82b807373
Liv <jcox0893@live.com>
2026-04-21 11:25:47 -0500

    lesson12part7

--- jobs/models.py
+++ jobs/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-
-# Create your models here.
 from datetime import datetime
-
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 from django.db import models
+import filetype
 
 def validate_future_date(value):
     if value < datetime.now().date():
         raise ValidationError(
             message=f'{value} is in the past.', code='past_date'
         )
+
+def validate_pdf(value):
+    kind = filetype.guess(value)
+    if not kind or kind.mime != 'application/pdf':
+        raise ValidationError("That’s not a PDF file.")
 
 class Job(models.Model):
     title = models.CharField(max_length=200)
@@ -45,7 +48,8 @@ class Applicant(models.Model):
     desired_hourly_wage = models.DecimalField(max_digits=5, decimal_places=2)
     cover_letter = models.TextField()
     resume = models.FileField(
-    upload_to='private/resumes', blank=True, help_text='PDFs only')
+    upload_to='private/resumes', blank=True, help_text='PDFs only',
+    validators=[validate_pdf])
     confirmation = models.BooleanField()
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
