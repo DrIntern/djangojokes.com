1ed1f9f5f26b7744bf74f48d472b7d727adf0f6f
Liv <jcox0893@live.com>
2026-04-22 10:59:11 -0500

    lesson15part4

--- jokes/urls.py
+++ jokes/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('joke/<slug>/update/', JokeUpdateView.as_view(), name='update'),
     path('joke/<slug>/vote/', vote, name='ajax-vote'),
     path('', JokeListView.as_view(), name='list'),
+    path('category/<slug>/', JokeListView.as_view(), name='category'),
+    path('tag/<slug>/', JokeListView.as_view(), name='tag'),
+    path('creator/<username>/', JokeListView.as_view(), name='creator'),
 ]
